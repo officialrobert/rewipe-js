bbb3d54715c0b755360fce668a20a4ce4df5c96d
officialrobert <officialrobert.espina9627@gmail.com>
2023-08-18 17:09:55 +0800

    feat: package export

---
+++ core/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'rewipe-js' {
+  export function config(params: IRewipeCoreConfig): void;
+
+  export function run(params: IRewipeRunParams): Promise<void>;
+
+  export function end(params: IRewipeEndParams): Promise<void>;
+
+  export function getEvent(eventName: string): IRewipeEvent[];
+}
