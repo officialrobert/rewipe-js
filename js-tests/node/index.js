995cc13aa883e3aa81662a9ded16b5ab94211040
officialrobert <officialrobert.espina9627@gmail.com>
2023-08-22 14:49:01 +0800

    test: testMemoryLeak()

--- js-tests/node/index.js
+++ js-tests/node/index.js
@@ -3,10 +3,9 @@ const {
   run,
   end,
   getEvent,
-  getEventMemoryInsights,
-  RewipeStorage,
   trackMemoryAndPromise,
-} = require('rewipe-js');
+  testMemoryLeak,
+} = require('../../dist');
 const { timeout } = require('./utils');
 
 const start = async (iteration = 1) => {
@@ -30,12 +29,11 @@ const start = async (iteration = 1) => {
 
   await end({ id, eventName });
 
-  const memInfo = getEvent(eventName);
-
-  console.log('evt id', id, 'memInfo', memInfo);
-  console.log('\n\n');
-  console.log(getEventMemoryInsights(memInfo[0]));
-  console.log('\n\n');
+  // const memInfo = getEvent(eventName);
+  // console.log('evt id', id, 'memInfo', memInfo);
+  // console.log('\n\n');
+  // console.log(getEventMemoryInsights(memInfo[0]));
+  // console.log('\n\n');
 };
 
 const testTrackMemoryAndPromise = async () => {
@@ -56,11 +54,25 @@ try {
     await start(1);
     await timeout(1_500);
     await start(1);
-    await timeout(500);
-    await start(2);
     await testTrackMemoryAndPromise();
 
-    console.log(RewipeStorage());
+    const { memoryConsumed, memoryInsights } = await testMemoryLeak(
+      async () => {
+        let obj1 = {};
+        let obj2 = {};
+
+        obj1 = null;
+        obj2 = null;
+
+        obj1 = { test: 1 };
+        obj2 = { test: 1 };
+      }
+    );
+
+    console.log('memoryConsumed:', memoryConsumed);
+    console.log('\n');
+    console.log('memoryInsights:\n');
+    console.log(memoryInsights);
   })();
 } catch (err) {
   console.log(err?.message);
